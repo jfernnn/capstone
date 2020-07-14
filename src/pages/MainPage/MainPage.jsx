1609255ada8d20285b636380daf6da765dff8454
Joshua Fernelius <joshfern24@50-250-158-121-static.hfc.comcastbusiness.net>
2020-07-14 17:02:11 -0600

    Basic User model

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const MainPage = (props) => {
     return (
-        <p>U made it</p>
+        <div>
+            <p>U made it</p>
+            <Link to='/options'>Options</Link>
+        </div>
     );
 };
 
--- src/pages/OptionsPage/OptionsPage.jsx
+++ src/pages/OptionsPage/OptionsPage.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const OptionsPage = (props) => {
     return (
-        <p>Look at u</p>
+        <div>
+            <Link to='/'>Home</Link>
+            <p>Look at u</p>
+        </div>
     );
 };
 
