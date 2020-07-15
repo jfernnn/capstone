080aa354c4f068bc1b905c5534053365240da746
Joshua Fernelius <joshfern24@Joshuas-MacBook-Pro.local>
2020-07-15 15:04:33 -0600

    Login and Signup functionality

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -4,18 +4,35 @@ import './App.css';
 import MainPage from '../MainPage/MainPage'
 import OptionsPage from '../OptionsPage/OptionsPage'
 import { Route, Switch } from 'react-router-dom';
+import SignupPage from '../SignupPage/SignupPage';
+import LoginPage from '../LoginPage/LoginPage';
+import userService from '../../utils/userService';
 
 class App extends Component {
+  constructor() {
+    super();
+
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">Welcome to Hell</header>
+        <header className="App-header">Songs Songs Songs</header>
         <Switch>
           <Route exact path='/' render={() =>
             <MainPage />
           }/>
           <Route exact path='/options' render={() =>
             <OptionsPage />
+          }/>
+          <Route exact path='/signup' render={({ history }) => 
+            <SignupPage 
+              history={history}
+            />
+          }/>
+          <Route exact path='/login' render={({ history }) => 
+            <LoginPage
+              history={history}
+            />
           }/>
         </Switch>
       </div>
--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import NavBar from '../../components/NavBar/NavBar';
 
 const MainPage = (props) => {
     return (
         <div>
+            <NavBar />
             <p>U made it</p>
             <Link to='/options'>Options</Link>
         </div>
